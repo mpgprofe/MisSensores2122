2d13339ba0c43dc694321870ed180cae1055ae84
Manuel Profesor <mpgprofe@iesflorenciopintado.es>
2021-11-08 15:00:26 +0100

    sensores y vibrador

--- app/src/main/java/com/example/missensores2122/MainActivity.java
+++ app/src/main/java/com/example/missensores2122/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.missensores2122;
 
+import static android.hardware.SensorManager.SENSOR_ALL;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView textView, textView2;
+    SensorManager sensorManager;
+    Vibrator vibrator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> list = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        String cadena = "";
+
+        for (Sensor s : list) {
+            cadena += "Sensor: " + s.getName() + " Tipo: " + s.getStringType() + " Fabricante: " + s.getVendor() + "\n";
+
+        }
+        textView.setText(cadena);
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),SensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            textView2.setText("Valor: " + event.values[0]);
+
+            if (event.values[0]==0){
+                vibrator.vibrate(500);
+            }
+        } else if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            textView2.setText("Luminosidad: " + event.values[0]);
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
